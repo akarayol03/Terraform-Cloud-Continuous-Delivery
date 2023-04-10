ce0e88ebd1650be6348f5ee6fdc3a244d7b0371e
pamidu.ruhunage / Ahmet Karayol <akarayol03@gmail.com>
2023-04-10 18:20:51 -0500

    Added

---
+++ Terraform-Cloud/Modules/LoadBalancer/main.tf
@@ -0,0 +1,41 @@
+resource "aws_lb" "tfcloud_lb" {
+  name               = "tfcloud-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.web_sg]
+  subnets            = tolist(var.public_subnet)
+
+  depends_on = [
+    var.database_asg
+  ]
+}
+
+resource "aws_lb_target_group" "tfcloud_tg" {
+  name     = "tfcloud-lb-tg-${substr(uuid(), 0, 5)}"
+  protocol = var.tg_protocol
+  port     = var.tg_port
+  vpc_id   = "var.vpc_id"
+
+  health_check {
+    healthy_threshold   = var.lb_healthy_threshold
+    unhealthy_threshold = var.lb_unhealthy_threshold
+    timeout             = var.lb_timeout
+    interval            = var.lb_interval
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        =[name]
+  }
+}
+
+resource "aws_lb_listener" "tfcloud_lb_listener" {
+  load_balancer_arn = aws_lb.tfcloud_lb.arn
+  port              = var.listener_port
+  protocol          = var.listener_protocol
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tfcloud_tg.arn
+  }
+}
---
+++ Terraform-Cloud/Modules/LoadBalancer/variables.tf
@@ -0,0 +1,12 @@
+variable "tg_port" {}
+variable "tg_protocol" {}
+variable "lb_healthy_threshold" {}
+variable "lb_unhealthy_threshold" {}
+variable "lb_timeout" {}
+variable "lb_interval" {}
+variable "vpc_id" {}
+variable "database_asg" {}
+variable "public_subnet" {}
+variable "web_sg" {}
+variable "listener_protocol" {}
+variable "listener_port" {}
